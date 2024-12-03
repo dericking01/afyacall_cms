4fa455a78bf17bbba35f148d06cf60be4701081c
MUHAMMAD MWINCHANDE <ammwinchande@gmail.com>
2024-12-03 09:34:35 +0300

    refactor: return the code for sending OTP

--- app/Models/User.php
+++ app/Models/User.php
@@ -69,21 +69,19 @@ class User extends Authenticatable
         $receiverNumber = auth()->user()->mobile;
         $message = 'Your Afyacall Login OTP code is ' . $code;
 
-        logger()->info($message);
-
-        //        try {
-        //            $client = new Client();
-        //            $client->request('GET', 'http://192.168.1.10:6013/cgi-bin/sendsms', [
-        //                'query' => [
-        //                    'username' => 'afya',
-        //                    'password' => 'Afya4017',
-        //                    'from' => '15723',
-        //                    'to' => '+' . $receiverNumber,
-        //                    'text' => $message,
-        //                ]
-        //            ]);
-        //        } catch (Exception $e) {
-        //            logger()->error('OTP sending failure', ['exception' => $e]);
-        //        }
+        try {
+            $client = new Client();
+            $client->request('GET', 'http://192.168.1.10:6013/cgi-bin/sendsms', [
+                'query' => [
+                    'username' => 'afya',
+                    'password' => 'Afya4017',
+                    'from' => '15723',
+                    'to' => '+' . $receiverNumber,
+                    'text' => $message,
+                ]
+            ]);
+        } catch (Exception $e) {
+            logger()->error('OTP sending failure', ['exception' => $e]);
+        }
     }
 }
